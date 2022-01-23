f11bac0e2beecd8700ad6d38409ef0e7437f9b6c
Amit Yadav <amit.yadav.iitr@gmail.com>
2022-01-23 12:31:25 +0000

    avoid circular import

--- teal/__init__.py
+++ teal/__init__.py
@@ -3,5 +3,4 @@
 This module contains a number of TensorFlow layers specifically
 written to be used with audio data
 """
-from teal import augment, feature
 from teal.augment.augment import AugmentationLayer
